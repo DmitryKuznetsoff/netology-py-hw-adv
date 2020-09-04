e763e0a8f3b832d6f4bf3e64c64d83bbc5aa9005
DmitryKuznetsoff <kuznetsoffd90@gmail.com>
2020-09-04 18:04:55 +0300

    hw#5 regexp

--- hw5_regexp/regexp.py
+++ hw5_regexp/regexp.py
@@ -1,49 +1,115 @@
 import csv
-
-# import re
-
-with open("phonebook_raw.csv", encoding='utf-8') as f:
-    rows = csv.reader(f, delimiter=",")
-    contacts_list = list(rows)
+import re
+from typing import Tuple
 
 
-def get_full_name():
-    # собираем фамилию, имя и отчество в строку и пишем в отдельный список contacts:
-    full_name = [f'{contacts[0]} {contacts[1]} {contacts[2]}' for contacts in contacts_list]
-    # разбиваем строки с ФИО по пробелу и получаем фамилию, имя и отчество в отдельных полях:
-    full_name = list(map(lambda x: x.strip().split(' '), full_name))
-    return full_name
+def get_full_name(lastname: str, firstname: str, surname) -> Tuple[str, str, str]:
+    name_pattern = re.compile(r'^(?P<lastname>\w+)\s?(?P<firstname>\w+)\s?(|(?P<surname>\w+))\s*$')
+    # собираем фамилию имя и отчество в строку:
+    full_name = ' '.join([lastname, firstname, surname])
+    matched_name = name_pattern.match(full_name).groupdict()
+    # выделяем группы, необходимые для форматирования ФИО:
+    lastname = matched_name['lastname']
+    firstname = matched_name['firstname']
+    surname = matched_name['surname']
+
+    return lastname, firstname, surname or ''
 
 
-def get_phones():
-    # убираем из телефонов все спецсимолы и пишем их в отдельный список phones:
-    phones = [''.join(list(filter(str.isalnum, phone[5]))) for phone in contacts_list]
+def get_phones(phone: str) -> str:
+    phone_pattern = re.compile(r'^(?P<country>[+7|8]+)\s?[(]?'
+                               r'(?P<code>\d{3})[/)|-]?\s?'
+                               r'(?P<group1>\d{3})[-]?'
+                               r'(?P<group2>\d{2})[-]?'
+                               r'(?P<group3>\d{2})\s?[(]?'
+                               r'(?P<add>\w{3}\.)?\s?'
+                               r'(?P<add_code>\d{4})?[/)]?$'
+                               )
 
-    for i in range(1, len(phones)):
-        # разбиваем номер телефона на группы:
-        code = phones[i][1:4]
-        g1 = phones[i][4:7]
-        g2 = phones[i][7:9]
-        g3 = phones[i][9:11]
-        add = phones[i][11:14] + '.' + phones[i][14:]
-        if phones[i] and 'доб' in phones[i]:
-            phones[i] = f'+7({code}){g1}-{g2}-{g3} {add}'
-        elif phones[i]:
-            phones[i] = f'+7({code}){g1}-{g2}-{g3}'
-    return phones
+    if phone:
+        matched_phone = phone_pattern.match(phone).groupdict()
+        # выделяем группы, необходимые для форматирования номера:
+        code = matched_phone['code']
+        group1 = matched_phone['group1']
+        group2 = matched_phone['group2']
+        group3 = matched_phone['group3']
+        add = matched_phone['add']
+        add_code = matched_phone['add_code']
+        # проверка на наличие добавочного номера:
+        if add:
+            # формируем из групп строку с номером по формату:
+            return f'+7({code}){group1}-{group2}-{group3} {add}{add_code}'
+        else:
+            return f'+7({code}){group1}-{group2}-{group3}'
+    else:
+        # если телефон не указан возвращаем пустую строку:
+        return ''
 
 
-name = get_full_name()
-phone = get_phones()
-# присоединяем исправленные ФИО и номера телефонов к списку контактов и формируем fixed_contacts_list:
-# fixed_contacts_list = []
-# for i in range(len(contacts_list)):
-#     fixed_contacts_list.append(name[i] + contacts_list[i][3:5] + [phone[i]] + [contacts_list[i][6]])
-# print(*fixed_contacts_list, sep='\n')
-print(*phone, sep='\n')
+def fix_contacts() -> list:
+    """
+    с помощью функций get_full_name и get_phones форматирует поля с ФОИ и номера телефонов
+    """
+    result = []
+    for contact in contacts_list:
+        lastname, firstname, surname = get_full_name(contact['lastname'],
+                                                     contact['firstname'],
+                                                     contact['surname']
+                                                     )
+        phone = get_phones(contact['phone'])
+        result.append({**contact,
+                       'lastname': lastname,
+                       'firstname': firstname,
+                       'surname': surname,
+                       'phone': phone}
+                      )
+    return result
 
-# код для записи файла в формате CSV
-# with open("fixed_phonebook.csv", "w") as f:
-#     datawriter = csv.writer(f, delimiter=',')
-#     # Вместо contacts_list подставьте свой список
-#     datawriter.writerows(contacts_list)
+
+def merge_duplicates(contacts: list) -> dict:
+    """
+    принимает на вход результат функции fix_contacts и мёрджит дублирующиеся строки:
+    """
+    result = {}
+    for person in contacts:
+        # формируем структуру словаря merged_contacts:
+        # ключом будет кортеж (имя, фамилия), значением - список всех остальных полей
+        person_key = (person['lastname'], person['firstname'])
+        person_values = [person['surname'],
+                         person['organization'],
+                         person['position'],
+                         person['phone'],
+                         person['email']
+                         ]
+        # если в merged_contacts уже есть текущий ключ - итерируемся по списку его значений
+        # и мёрджим недостающие поля
+        if person_key in result:
+            for i in range(len(person_values)):
+                if not result[person_key][i] and person_values[i]:
+                    result[person_key][i] = person_values[i]
+        else:
+            result.update({person_key: person_values})
+    return result
+
+
+if __name__ == '__main__':
+    with open("phonebook_raw.csv", encoding='utf-8') as f:
+        rows = csv.DictReader(f, delimiter=",")
+        contacts_list = list(rows)
+
+    fixed_contacts = fix_contacts()
+    merged_contacts = merge_duplicates(fixed_contacts)
+
+    # заголовки для записи в csv:
+    headers = list(contacts_list[0].keys())
+
+    fixed_contacts_list = list()
+    fixed_contacts_list.append(headers)
+
+    for k, v in merged_contacts.items():
+        x = list(k) + list(v)
+        fixed_contacts_list.append(x)
+
+    with open("fixed_phonebook.csv", "w", encoding='utf-8') as f:
+        datawriter = csv.writer(f, delimiter=',')
+        datawriter.writerows(fixed_contacts_list)
